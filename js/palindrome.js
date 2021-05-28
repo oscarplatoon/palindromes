4976293d8ec12866cee2c010cd184a179f2f9e9a
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-05-28 16:30:08 -0500

    Shawn's JS solution without unittests

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,8 +1,8 @@
 // check input type of WORD
 // if WORD is integer, convert to string
-// Remove all non alpha-numeric chars from WORD
-// reverse WORD and store in REVERSEWORD
-// compare WORD and REVERSEWORD and return the result
+// Remove all non alpha-numeric chars from WORD and store in CLEANWORD
+// reverse CLEANWORD and store in REVERSEWORD
+// compare CLEANWORD and REVERSEWORD and return the result
 // exports.
 
 let palindrome = function(word) {
@@ -11,8 +11,18 @@ let palindrome = function(word) {
     }
    
     let cleanWord = word.replace(/\W/g, '')
+    cleanWord = cleanWord.toLowerCase();
 
-    return cleanWord;
+    let reverseWordArray = [];
+    
+    for (let i = 0; i < word.length; i++){
+      reverseWordArray.push(cleanWord[i]);
+    }
+
+    let reverseWord = reverseWordArray.join("");
+
+    return reverseWord === cleanWord;
+
 };
 
-console.log(palindrome("ra7c-e car"))
+console.log(palindrome("Noon"))
