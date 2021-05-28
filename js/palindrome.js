681d0d35f134e227927b98f41724f68cc3cd1bad
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-05-28 16:23:30 -0500

    Shawn's partial solution in javascript to palindrome

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,18 @@
-exports.palindrome = function(word) {
+// check input type of WORD
+// if WORD is integer, convert to string
+// Remove all non alpha-numeric chars from WORD
+// reverse WORD and store in REVERSEWORD
+// compare WORD and REVERSEWORD and return the result
+// exports.
 
+let palindrome = function(word) {
+    if (typeof(word) === "number") {
+      word = toString(word);
+    }
+   
+    let cleanWord = word.replace(/\W/g, '')
+
+    return cleanWord;
 };
+
+console.log(palindrome("ra7c-e car"))
