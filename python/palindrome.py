ac54bf5f8a700d9762aa703a79d6d77ffdd4af70
SarahShinespark <sarahshinespark@users.noreply.github.com>
2021-05-28 20:10:03 +0000

    Finished palindromes in Python

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,11 @@
 def palindrome(word):
     # Write code here
+    word = str(word).lower()
+    banned = "!@#$%^&*()_+-=[]{};':,./<>? "
+    
+    # Remove punctuation through an overly complicated method I totally didn't look up online
+    clean = ''.join(filter(lambda x: x not in list(banned), word))
+    nealc = clean[::-1]  #Reverses the string
+    if clean == nealc: return True
+    return False
+    
