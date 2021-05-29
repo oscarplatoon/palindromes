ee5caf40331fce7b438326d973d2e25ede900147
AlexanderS68 <gruntmedic68@gmail.com>
2021-05-29 12:34:21 -0400

    palindrome py solution

--- python/palindrome_spec.py
+++ python/palindrome_spec.py
@@ -11,9 +11,9 @@ class TestingOfPalindrome(unittest.TestCase):
     print(palindrome(123) == False)
     print(palindrome('bomb') == False)
 
+  def test_of_a_phrase(self):
+    print(palindrome('Sore was I ere I saw Eros.') == True)
+    print(palindrome('A man, a plan, a canal -- Panama') == True)
+
 if __name__ == '__main__':
   unittest.main()
-
-#print("The following should be True if you're trying to do the extra portion of this challenge")
-#print(palindrome('Sore was I ere I saw Eros.') == True)
-#print(palindrome('A man, a plan, a canal -- Panama') == True)
