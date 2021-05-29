a634fc730e4da8b6b28b85edfeaecb873973c68a
AlexanderS68 <gruntmedic68@gmail.com>
2021-05-29 12:23:29 -0400

    modifiy to account for integers

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,18 +1,22 @@
 import string
 
 def palindrome(word):
+  #check if word is an integer
+  while isinstance(word, int):
+    int_to_str = f'{word}'
+    if int_to_str == int_to_str[::-1]:
+      return True
+    elif int_to_str != int_to_str[::-1]:
+      return False
   #Removes any punctuations in a possible string
   punct_check = word.translate(str.maketrans('', '', string.punctuation))
   #Lowercase String prior to comparison/persisent copy of word
   word_lowered = punct_check.lower()
   #Reverse of word prior to comparison
   word_reversed = word_lowered[::-1]
+
   if word_reversed == word_lowered:
     return True
   else:
     return False
    
-
-
-
-print(palindrome("Sore was I ere I saw Eros."))
