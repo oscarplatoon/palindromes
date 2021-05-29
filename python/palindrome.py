e3fb2740edd2456c7d51d20c7ae84be79b5b833e
AlexanderS68 <gruntmedic68@gmail.com>
2021-05-29 10:31:21 -0400

    palindrome with sentence check function

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,18 @@
+import string
+
 def palindrome(word):
-    # Write code here
+  #Removes any punctuations in a possible string
+  punct_check = word.translate(str.maketrans('', '', string.punctuation))
+  #Lowercase String prior to comparison/persisent copy of word
+  word_lowered = punct_check.lower()
+  #Reverse of word prior to comparison
+  word_reversed = word_lowered[::-1]
+  if word_reversed == word_lowered:
+    return True
+  else:
+    return False
+   
+
+
+
+print(palindrome("Sore was I ere I saw Eros."))
