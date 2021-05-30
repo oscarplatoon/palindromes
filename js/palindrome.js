a60f90a788e1aea9cd08d3cf5c999adc336a9de3
jung341416 <jung341416@gmail.com>
2021-05-29 23:42:09 -0400

    forgot number case

--- js/palindrome.js
+++ js/palindrome.js
@@ -2,6 +2,7 @@ const palindrome = (word) => {
   // create empty string named palindrome
   let palindromeStr = "";
   let alpahbets = "abcdefghijklmnopqrstuvwxyz";
+  let numbers = "1234567890";
   // if the input word is number make it to string so that for loop can be used
   if(typeof word === "number") {
     word = String(word);
@@ -10,12 +11,13 @@ const palindrome = (word) => {
   // ignore case sensitive
   // filter out special chars
   word = word.split(' ').join('').toLowerCase();
-  let filterOutSpecialWord = word.replace(/[^a-zA-Z]/gm,"")
+  let filterOutSpecialWord = word.replace(/[^a-zA-Z1-9]/gm,"")
   // loop through starting last index decrement 1
   // and save each index element to palindrome word
   for(let i = word.length - 1; i >= 0 ; i --) {
+    let alphabetsOrNums = (alpahbets.indexOf(word[i]) > -1 || numbers.indexOf(word[i]) > -1);
     // only alphabets to be added
-    if(alpahbets.indexOf(word[i]) > -1) {
+    if(alphabetsOrNums) {
     palindromeStr += word[i];
     }
   }
@@ -24,5 +26,5 @@ const palindrome = (word) => {
   // else false.
   return (palindromeStr === filterOutSpecialWord ?  true : false);
 }
-palindrome("Sore was I ere I saw Eros.")
+
 module.exports = palindrome;
--- js/palindrome.test.js
+++ js/palindrome.test.js
@@ -5,4 +5,6 @@ const palindrome = require('./palindrome');
 test('will return true if the word is plaindrome', () => {
   expect(palindrome("Sore was I ere I saw Eros.")).toBe(true);
   expect(palindrome("A man, a plan, a canal -- Panama")).toBe(true);
+  expect(palindrome(1221)).toBe(true);
+  expect(palindrome(1_2_2_2_1)).toBe(true);
 })
