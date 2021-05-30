b305a1949238dadc8beb6e70b0632018b3e817bc
jung341416 <jung341416@gmail.com>
2021-05-29 22:56:07 -0400

    pseudo code

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,13 @@
-exports.palindrome = function(word) {
- return word;
-};
+const palindrome = (word) => {
+  return word;
+}
+module.exports = palindrome;
+// create empty string named palindrome
+// if the input word is make it to string so that for loop can be used
+// omit space
+// ignore case sensitive
+// loop through starting last index decrement 1
+ // and save each index element to palindrome word
+ // compare palindrome and input word
+ // return true if match 
+ // else false.
--- js/palindrome.test.js
+++ js/palindrome.test.js
@@ -1,8 +1,7 @@
 // Can you translate this driver code to unit tests?
 
 const { expect } = require('@jest/globals');
-const { test } = require('jest-circus');
 const palindrome = require('./palindrome');
 test('will return true if the word is plaindrome', () => {
-  expect(palindrome('abc')).tobe('abc');
+  expect(palindrome('abc')).toBe('abc');
 })
