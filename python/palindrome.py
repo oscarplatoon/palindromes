150179b5437e1bfacd34ee7bf99c11fb0bf659ab
Marc Stanley <marcson909@gmail.com>
2021-05-31 18:00:07 -0500

    completed py using comparison of left and right index values and iterating inwards

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,3 +1,16 @@
 def palindrome(word):
     # Write code here
-    return False
+    if word == "":
+        return True
+    else:
+        lowered_word = word.lower()
+        
+        left = 0
+        right = len(lowered_word)-1
+
+        while(left < right):
+            if lowered_word[left] != lowered_word[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
